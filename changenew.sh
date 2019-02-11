58a748446a316b7dd14da95d6b263582412b045b
Rudolf Schlatte <rudi@relax.ifi.uio.no>
2019-02-11 12:49:09 +0100

    remove one-shot syntax fixup script

--- changenew.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/csh
-
-foreach i (`find . -name "*.abs"`)
-  cat $i | sed s/new\ cog/nglobal/g | sed s/new/nlocal/g | sed s/nglobal/new/g |  sed s/nlocal/new\ local/g > $i.tmp
-  mv -f $i.tmp $i
-end
